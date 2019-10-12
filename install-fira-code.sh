9aca5f6ce63f115b0e46c493386839bd9b4fd9b4
Vladyslav Pekker <agilesteel@gmail.com>
2019-10-12 20:57:21 +0200

    Update install-fira-code.sh

--- install-fira-code.sh
+++ install-fira-code.sh
@@ -6,17 +6,14 @@ rm $0
 # install fira code
 fonts_dir="${HOME}/.local/share/fonts"
 if [ ! -d "${fonts_dir}" ]; then
-    echo "mkdir -p $fonts_dir"
     mkdir -p "${fonts_dir}"
 fi
 
 for type in Bold Light Medium Regular Retina; do
     file_path="${HOME}/.local/share/fonts/FiraCode-${type}.ttf"
     file_url="https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true"
-    echo "wget -O $file_path $file_url"
     wget -qO "${file_path}" "${file_url}"
 done
 
-echo "fc-cache -f"
-echo "this should take half a minute or so..."
+echo "Updating the font cache. This should take half a minute or so..."
 fc-cache -f
