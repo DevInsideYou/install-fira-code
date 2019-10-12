ae5e1face01cf27dcf27e6a4ecb08c6b01209ea4
Vladyslav Pekker <agilesteel@gmail.com>
2019-10-12 20:55:36 +0200

    Update install-fira-code.sh

--- install-fira-code.sh
+++ install-fira-code.sh
@@ -10,11 +10,11 @@ if [ ! -d "${fonts_dir}" ]; then
     mkdir -p "${fonts_dir}"
 fi
 
-for type in Bold Medium Regular Retina; do
+for type in Bold Light Medium Regular Retina; do
     file_path="${HOME}/.local/share/fonts/FiraCode-${type}.ttf"
     file_url="https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true"
     echo "wget -O $file_path $file_url"
-    wget -O "${file_path}" "${file_url}"
+    wget -qO "${file_path}" "${file_url}"
 done
 
 echo "fc-cache -f"
